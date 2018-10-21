43ce486eb51ea2985e4106b324bdb63ae0d45296
Skyler <skylerwphilliptstx@gmail.com>
2018-10-20 21:16:36 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 def prime?(number)
-  if number == 1
-    return false
-  elsif number == 0
+  if number == 1 || number == 0 || number < 0
     return false
   elsif number == 2
     return true
-  elsif number < 0
-    return false 
   else
   array_a = (2...number).to_a
    array_a.each do |int|
-     if number % int == 0
+      if number % int == 0
        return false
      end
    end
